60f97b471e6c846821aea70e3709461cd1dc391a
DiasTime <36094372+DiasTime@users.noreply.github.com>
2024-02-15 17:58:13 +0600

    main_v2_db_deleted

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_from_directory,send_file, url_for
+from flask import Flask, render_template, request, send_from_directory, send_file, url_for
 import os
 from docx import Document
 from pptx import Presentation
@@ -6,83 +6,35 @@ from PIL import Image
 from io import BytesIO
 import base64
 import fitz
-
-
 import yadisk
-import mysql.connector
-
-
-
 
 app = Flask(__name__)
-
-UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-# Подключение к MySQL базе данных
-db = mysql.connector.connect(
-    host="192.168.68.102",
-    user="python",
-    password="12345",
-    database="files"
-)
-cursor = db.cursor()
 
 # Токен для доступа к Яндекс.Диску
 y = yadisk.YaDisk(token="y0_AgAAAAAl5jRpAAtKfAAAAAD6_t3BAABRqHACUIJMrJ4PRAckXHu7iVOojw")
 
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 def list_files_on_yadisk(folder_path):
     files = y.listdir(folder_path)
-    # Преобразуем генератор в список кортежей
     files_list = [(file.name, file.path) for file in files]
     return files_list
 
-
-# Получение списка файлов из базы данных
-def list_files_from_database():
-    cursor.execute("SELECT * from files")
-    files = cursor.fetchall()
-    return files
-
-# Функция для сохранения файла на Яндекс.Диск
 def save_to_yadisk(file_path):
-    # Проверяем, начинается ли имя файла с префикса temp_
-    if not os.path.basename(file_path).startswith('temp_'):
-        # Если имя файла не начинается с префикса temp_, загружаем его на Яндекс.Диск
-        y.upload(file_path, f'/uploads/{os.path.basename(file_path)}')
-
+    y.upload(file_path, f'/uploads/{os.path.basename(file_path)}')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     yadisk_files = list_files_on_yadisk('/uploads')
-    db_files = list_files_from_database()
 
     if request.method == 'POST':
         file = request.files['file']
         if file:
             filename = file.filename
-
-            # Проверяем, есть ли файл уже на Яндекс.Диске
-            existing_file = next((f for f in yadisk_files if f[1] == filename), None)
-            if existing_file:
-                # Если файл уже есть на Яндекс.Диске, создаем ссылку на него
-                file_url = url_for('open_from_yadisk', file_path=existing_file[1])
-            else:
-                # Если файла нет на Яндекс.Диске, сохраняем его туда
-                file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-                file.save(file_path)
-
-                # Проверяем, начинается ли имя файла с префикса temp_
-                if not filename.startswith('temp_'):
-                    # Если нет, добавляем информацию о файле в базу данных
-                    save_to_yadisk(file_path)
-                    cursor.execute("INSERT INTO files (filename, file_path) VALUES (%s, %s)", (filename, file_path))
-                    db.commit()
-
-                    file_url = url_for('open_from_yadisk', file_path=f'/uploads/{filename}')
-                else:
-                    # Если имя файла начинается с префикса temp_, не добавляем его в базу данных
-                    file_url = None
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            save_to_yadisk(file_path)
 
             if filename.endswith('.docx'):
                 text = process_word(file_path)
@@ -101,26 +53,15 @@ def index():
                 slides_data = [{"text": text, "images": []}]
                 return render_template('pptx_template.html', slides=slides_data, filename=filename)
             
-    return render_template('index.html', yadisk_files=yadisk_files, db_files=db_files)
+    return render_template('index.html', yadisk_files=yadisk_files)
 
 @app.route('/clear_data', methods=['GET'])
 def clear_data():
-    # Проверяем, пришло ли ключевое слово 'clear_all_data' в параметрах запроса
     if request.args.get('key') == 'clear_all_data':
-        # Очищаем данные из таблицы files в базе данных
-        cursor.execute("DELETE FROM files")
-        db.commit()
-        
-        # Удаляем все файлы в папке uploads на Яндекс.Диске
-        yadisk_files = list_files_on_yadisk('/uploads')
-        for file in yadisk_files:
-            y.remove(file[1])
-        
+        y.remove('/uploads')
         return "Data cleared successfully."
     else:
         return "Invalid key."
-
-
 
 @app.route('/open_from_yadisk/<path:file_path>')
 def open_from_yadisk(file_path):
@@ -130,33 +71,14 @@ def open_from_yadisk(file_path):
 
     return send_file(temp_file, as_attachment=True)
 
-@app.route('/open_from_db/<path:file_path>')
-def open_from_db(file_path):
-    encodings = ['utf-8', 'latin-1']  # список возможных кодировок
-    for encoding in encodings:
-        try:
-            with open(file_path, 'rb') as file:
-                file_data = file.read().decode(encoding)
-            return file_data
-        except UnicodeDecodeError:
-            continue
-    return "Failed to decode file"
-
 @app.route('/files', methods=['GET'])
 def list_files():
-    cursor.execute("SELECT id, filename, file_path FROM files")
-    files = cursor.fetchall()
-    return render_template('files.html', files=files)
+    yadisk_files = list_files_on_yadisk('/uploads')
+    return render_template('files.html', files=yadisk_files)
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
-@app.route('/apply_style', methods=['GET'])
-def apply_style():
-    style = request.args.get('style')
-    text = request.args.get('text')
-    return render_template('result.html', text=text, css_file=style)
 
 def process_word(file_path):
     document = Document(file_path)
@@ -167,20 +89,14 @@ def process_word(file_path):
 
 def process_pdf(file_path):
     images = []
-    filename = os.path.basename(file_path)  # Получаем имя файла из пути
+    filename = os.path.basename(file_path)
     with fitz.open(file_path) as pdf:
         for page in pdf:
-            # Получаем изображение страницы PDF в формате RGBA
             pixmap = page.get_pixmap(alpha=True)
-            # Создаем объект Image из библиотеки PIL на основе pixmap
             img = Image.frombytes("RGBA", [pixmap.width, pixmap.height], pixmap.samples)
-            # Создаем объект BytesIO для записи изображения
             image_stream = BytesIO()
-            # Сохраняем изображение в формате PNG в объект BytesIO
             img.save(image_stream, format="PNG")
-            # Получаем данные изображения в виде байтовой строки
             image_base64 = base64.b64encode(image_stream.getvalue()).decode('utf-8')
-            # Добавляем закодированное изображение в список
             images.append(image_base64)
     return filename, images
 
@@ -192,7 +108,7 @@ def process_pptx(file_path):
         for shape in slide.shapes:
             if hasattr(shape, "text"):
                 slide_data["text"] += shape.text + "\n"
-            elif shape.shape_type == 13:  # Код 13 означает изображение
+            elif shape.shape_type == 13:
                 image_stream = BytesIO()
                 img = shape.image
                 img_bytes = img.blob
@@ -204,3 +120,4 @@ def process_pptx(file_path):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
